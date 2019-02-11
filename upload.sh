8136b44e585c10a8f11944be3d953d9e9b043589
alugocp <alugocp@aim.com>
2019-02-10 19:15:43 -0500

    Uploaded to master branch

--- upload.sh
+++ upload.sh
@@ -4,5 +4,6 @@ git add .build
 printf "Please add a commit message: "
 read msg
 git commit -m "$msg"
-git subtree push -f --prefix .build origin master
+#git subtree push -f --prefix .build origin master
+git push origin `git subtree split --prefix .build master`:master --force
 [ $? = 0 ] && echo "Upload successful!"
